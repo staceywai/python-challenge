6898e41d01297d56758c9a32164fea7a7ec339b5
staceywai <staceywai@Staceys-MacBook-Pro.local>
2020-10-12 15:56:40 -0700

    PyPoll HW results

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -23,6 +23,8 @@ with open(csvpath) as csvfile:
     total_votes = 0 
     vote = 0
     candidate_list = {}
+    listofcandidates = []
+
 
    # Read each row of data after the header
     for row in electiondata:
@@ -31,40 +33,38 @@ with open(csvpath) as csvfile:
         # grab all candidate list
         candidate = row[2]
         if candidate not in candidate_list:
-            candidate_list.append(candidate)
-        else: candidate_list[candidate] +=1
-        print(candidate_list)
-        # if candidate_unique.get(row[2]):
-        #     candidate_unique[row[2]] +=1
-        # else: candidate_unique[row[2]] = 1
-    
+            candidate_list.update({candidate:1})
+            listofcandidates.append(candidate)
+        else: candidate_list[candidate] +=1 
+    print(f'candidate:',candidate_list[candidate])
+    print(listofcandidates)
+    print(listofcandidates[0])
+    candidate_count = len(listofcandidates)
+    print(len(listofcandidates))
 
 
-# for candidate_name in all_candidates
-#     print(candidate_name.items())
-# define variables
-# total_votes = 0
-# all_candidates_with_duplication = []
-# begin looping through and gathering the data
-#   for row in reader:
-#     total_votes += 1
-#     all_candidates_with_duplication.append(row[2])
-#     if candidate_totals.get(row[2]):
-#         candidate_totas(row[2]) +=1
-#     else: candidate_totals[row[2]] = 1
+#calculate values
+    # for candidate in candidate_list:
+    #     candidate_vote_total = candidate_list[candidate]
+    # print(f'candidate votes:', candidate_vote_total)
+    # candidate_percentage = candidate_vote_total/total_votes
+    # print(f'candidate percentage:', candidate_percentage)
 
-# # Print Summary table
-# for cancdidate_name, vote_total in candidate_totals:
-#         print(candidate_name.items:())
 
 print('Election Results')
-print('-------------------------')
 print(f'Total Votes: ', total_votes)
 print('-------------------------')
-print(f'Khan: 63.000% (2218231)')
-print(f'Correy: 20.000% (704200)')
-print(f'Li: 14.000% (492940)')
-print(f'O Tooley: 3.000% (105630)')
-print('-------------------------')
-print(f'Winner: ')
 
+print_count = -1
+for x in listofcandidates:
+    # define number of iterations of for loop
+    print_count += 1
+    print(f'',listofcandidates[print_count],":","{:0.2%}".format((candidate_list[listofcandidates[print_count]]/total_votes)),"(", candidate_list[listofcandidates[print_count]],")")
+    # listofcandidates[row],"{:0.2%}".format((candidate_list[listofcandidates[row]]/total_votes)), candidate_list[listofcandidates[row]])
+print('-------------------------')
+# print(f'Khan',"{:0.2%}".format((candidate_list['Khan']/total_votes)), candidate_list['Khan'])
+# print(f'Correy:', "{:0.2%}".format((candidate_list['Correy']/total_votes)), candidate_list['Correy'])
+# print(f'Li:', "{:0.2%}".format((candidate_list['Li']/total_votes)), candidate_list['Li'])
+# print(f'O Tooley:', "{:0.2%}".format((candidate_list["O'Tooley"]/total_votes)), candidate_list["O'Tooley"])
+# print('-------------------------')
+print(f'Winner:',max(candidate_list, key=candidate_list.get))
